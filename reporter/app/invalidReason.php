cc120576104a97f91ae83c51eae93cda1e82994e
JuicyLung91 <peterroj@Peters-MBP.fritz.box>
2018-08-22 18:49:49 +0200

    Function for CRUD invalidReasons

--- reporter/app/invalidReason.php
+++ reporter/app/invalidReason.php
@@ -7,4 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class invalidReason extends Model
 {
     //
+
+
+    /**
+     * create a reson
+     */
+    function createReson () {
+
+    }
+
+    /**
+     * read a resons
+     */
+    function readReason () {
+
+    }
+
+     /**
+      * update a reason
+      */
+      function updateReson () {
+
+    }
+
+
+    function deleteReson () {
+
+    }
+      /**
+       * delete a resons
+       */
 }
