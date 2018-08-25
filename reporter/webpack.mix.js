17d0f1dc1d03fec82b558728e49c332a4fc14b16
JuicyLung91 <peterroj@Peters-MBP.fritz.box>
2018-08-25 21:00:39 +0200

    Webpack Einstellunge

--- reporter/webpack.mix.js
+++ reporter/webpack.mix.js
@@ -11,5 +11,17 @@ let mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .options({
+    processCssUrls: false,
+    autoprefixer: {
+        options: {
+            browsers: [
+                'last 6 versions',
+            ]
+        }
+    }
+  });
+
