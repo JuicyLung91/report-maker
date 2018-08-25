16a60525e04124da9b46ae818ccda2fea14496cd
JuicyLung91 <peterroj@Peters-MBP.fritz.box>
2018-08-25 21:02:36 +0200

    add weeks controller

--- reporter/app/weeks.php
+++ reporter/app/weeks.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class weeks extends Model
 {
-    //
+    /**
+     * 
+     * add new week 
+     * @param $startdate $enddate drame date
+     * 
+     */
+    public function addNewWeek ($startdate, $enddate) {
+        
+    }
+
+    //return one week - get dates and invalid dates
+    
+    //get tasks for a specific week
+
+    //return weeks in a period
+
+    //return all weeks
 }
