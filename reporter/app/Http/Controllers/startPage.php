8d951cc76ecefc36819b438d48940950fb62de4f
JuicyLung91 <peterroj@Peters-MBP.fritz.box>
2018-08-25 21:03:31 +0200

    add startPage Controller

---
+++ reporter/app/Http/Controllers/startPage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class startPage extends Controller
+{
+    //
+    function startPage () {
+        //show days
+
+        //show scool days
+        //
+        return view('start');
+    }
+}
