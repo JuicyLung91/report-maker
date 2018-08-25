4c2e2fe5fda3f66a526b845328a4288cadff4406
JuicyLung91 <peterroj@Peters-MBP.fritz.box>
2018-08-25 23:54:15 +0200

    add create reason method

--- reporter/app/invalidReason.php
+++ reporter/app/invalidReason.php
@@ -3,17 +3,56 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use App\date;
+use App\weeks;
 
 class invalidReason extends Model
 {
     //
 
-
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
     /**
      * create a reson
      */
-    function createReson () {
+    public function createReson ($date, $reason) {
+      $dateFormat = 'Y-m-d';
+      $day = new Carbon;
+      $date = Carbon::createFromFormat($dateFormat, $date);
+      $date = $day->parse($date)->toDateString();
+      $newReason = new invalidReason;
+      if ( !$newReason->where('reason', $reason)->first() ) {
+        $newReason->reason = $reason;
+        $newReason->save();
+      }
+      $reasonInDB = $newReason->where('reason', $reason)->first();
+      $reasonInDB = $reasonInDB->IDinvalidReasons;
+      echo $reasonInDB.'<br>';
+      
+      $newDate = new date;
+      if ( !$newDate->where('date', $date)->first() ) {
+        $newDate->date = $date;
+        $newDate->save();
+      } 
+      $dateInDB = $newDate->where('date', $date)->first();
+      $dateInDB = $dateInDB->IDdates;
+      echo $dateInDB;
 
+
+      $daysUntilSunday = date::daysUntilLastWeekDay($date);
+      $sunday = Carbon::parse($date)->addDays($daysUntilSunday)->toDateString();
+      $sundayDateID = $newDate->where('date', $sunday)->first();
+      $sundayDateID = $sundayDateID->IDdates;
+      
+      if ($sundayDateID) {
+        $week = new weeks;
+        $week = $week->where('endDate', $sundayDateID)->first();
+        echo  $week;
+      }
+      
+      //@TODO
+      //look for date::daysUntilLastWeekDay add days to date and look for weeks based on the endDate and get id
     }
 
     /**
